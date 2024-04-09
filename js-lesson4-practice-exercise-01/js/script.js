8fe606b54df0db926954b7732b8f7c24cbcd57cd
Jack <155328654+Jack-N-S@users.noreply.github.com>
2024-04-09 19:52:42 -0400

    Added files and folders only to GitHub repo

---
+++ js-lesson4-practice-exercise-01/js/script.js
@@ -0,0 +1,17 @@
+var footer = document.querySelector("footer");
+footer.style.backgroundColor = "#d7ddf3";
+//console.log(footer.outerHTML);
+
+var express = document.querySelector(".tagline");
+express.innerText = "Express Yoself!";
+express.style.textTransform = "capitalize";
+//console.log(express.innerText);
+
+var address = document.querySelector("address");
+address.style.lineHeight = "1.5em";
+address.style.fontSize = "1.75em";
+//console.log(address.outerHTML);
+
+var everyDay = document.querySelector("h3");
+everyDay.innerHTML = "We're here for you <em>every day</em> of the week.";
+//console.log(everyDay.innerHTML);
